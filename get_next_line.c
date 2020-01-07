35a4e19118fa7eabc793d5958099203ef5bdff98
Adrien Baur <abaur@student.42.fr>
2020-01-07 14:33:54 +0100

    Added concatbuff
    Fix Norme.
    get_next_char now outputs EOF.

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,21 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 16:19:44 by abaur             #+#    #+#             */
-/*   Updated: 2020/01/07 11:16:32 by abaur            ###   ########.fr       */
+/*   Updated: 2020/01/07 14:30:06 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 
 /*
 ** Allocates a string capable of holding the entirety of the chained buffers,
 ** plus a Null terminator.
+** @param t_gnlbuffer* first The first link of the buffer chain
+** @return The allocated string, or NULL if the allocation fails.
 */
 
 static char	*bufmalloc(t_gnlbuffer *first)
@@ -28,17 +31,53 @@ static char	*bufmalloc(t_gnlbuffer *first)
 
 	i = 0;
 	last = first;
-	while(last->next)
+	while (last->next)
 	{
 		last = last->next;
 		i++;
 	}
 	len = i * BUFFER_SIZE;
 	i = 0;
-	while(last->content[i] && last->content[i] != '\n')
+	while (last->content[i] && last->content[i] != '\n')
 		i++;
 	len += i;
 	return (malloc(len + 1));
+}
+
+/*
+** Merge all chained buffers into a single string, and frees the buffer.
+** Buffers are freed even if the allocation fails.
+** @param t_gnlbuffer* first The first link of the buffer chain.
+** @return The concatenated string, or NULL if the allocation fails.
+*/
+
+static char	*concatbuff(t_gnlbuffer *first)
+{
+	t_gnlbuffer *current;
+	t_gnlbuffer *prev;
+	char		*result;
+	char		*cursor;
+	size_t		i;
+
+	result = bufmalloc(current);
+	i = 0;
+	cursor = result;
+	current = first;
+	while (current && current->content[i])
+	{
+		if (result)
+			*(cursor++) = current->content[i++];
+		if (!result || i == BUFFER_SIZE)
+		{
+			i = 0;
+			prev = current;
+			current = current->next;
+			free(prev);
+		}
+	}
+	if (result)
+		*cursor = '\0';
+	return (result);
 }
 
 /*
@@ -47,7 +86,7 @@ static char	*bufmalloc(t_gnlbuffer *first)
 ** @param char* dst The adress where to write the character.
 ** @return
 **  1 if a character is read
-**  0 upon reading EOF
+**  0 upon reading EOF, EOF is output to dst.
 ** -1 in case of error
 */
 
@@ -61,6 +100,8 @@ int			get_next_char(int fd, char *dst)
 	{
 		offset = 0;
 		readsize = read(fd, buffer, BUFFER_SIZE);
+		if (readsize == 0)
+			*dst = EOF;
 		if (readsize < 1)
 			return (readsize);
 	}
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,22 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 16:20:24 by abaur             #+#    #+#             */
-/*   Updated: 2020/01/07 10:51:33 by abaur            ###   ########.fr       */
+/*   Updated: 2020/01/07 13:16:42 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+
 # define BUFFER_SIZE 32
 
-typedef s_gnlbuffer t_gnlbuffer;
-struct s_gnlbuffer {
-	t_gnlbuffer *next;
-	char **content;
+typedef struct s_gnlbuffer	t_gnlbuffer;
+struct	s_gnlbuffer {
+	t_gnlbuffer	*next;
+	size_t		size;
+	char		content[BUFFER_SIZE];
 };
 
 int		get_next_char(int fd, char *dst);
